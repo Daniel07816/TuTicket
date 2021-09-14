7e75d9808b2252cc7ed121a48c1973dbc064383f
CarlosAlvarado-git <cmalvarado@ufm.edu>
2021-09-14 17:10:41 -0600

    Update database.js
    Se agregó la instancia de Amazon para poder levantar postgres y que todos los integrantes del grupo puedan conectarse a la misma base de datos sin que ellos deban obtener el archivo de  la base de datos e incluso usar un usuario distinto para entrar.

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "localhost",
+    host: "ec2-35-170-202-150.compute-1.amazonaws.com",
     user: "postgres",
     port: 5432,
-    password: "CharleiAlvSql",
-    database: "demoTuTicketdb"
+    password: "AdminCM21",
+    database: "postgres"
 
 })
 
