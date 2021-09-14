29be2bf71caca9ff721400774a9b78cda774ecbe
Javier Mazariegos <javiermazariegos@ufm.edu>
2021-09-14 17:43:51 -0600

    componente main
    Se paso el main de appComponent a mainComponent

--- ticket/src/app/app.module.ts
+++ ticket/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanezeraComponent } from './canezera/canezera.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CanezeraComponent
+    CanezeraComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
