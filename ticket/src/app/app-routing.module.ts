c9955e094e9d5d79c2f738b336e7dd748bd91c83
Javier Mazariegos <javiermazariegos@ufm.edu>
2021-09-16 08:22:18 -0600

    Se crearon las rutas
    Se crearon las rutas "/login" y "/registro"

--- ticket/src/app/app-routing.module.ts
+++ ticket/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'registro',
+    component: MainComponent
+  }
 ];
 
 @NgModule({
