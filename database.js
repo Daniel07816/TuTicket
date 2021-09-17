a16b45b4004b66708ed8377e1ca8164b057afa02
CarlosAlvarado-git <cmalvarado@ufm.edu>
2021-09-17 13:26:32 -0600

    Se actualizo la tabla para id incremental y así el id de usuario es único

--- database.js
+++ database.js
@@ -10,8 +10,13 @@ const client = new Client({
 })
 
 client.connect();
-
-client.query(`SELECT * FROM users_data`, (err, res)=>{
+//Información de usuario luego de crear cuenta. SELECT
+var nombre = 'Carlos'
+const SELECTQ = {
+    text: 'SELECT nombre, apellido, correo, edad, region FROM public.users_data WHERE nombre = ($1)',
+    values: [nombre]
+  }
+client.query(SELECTQ, (err, res)=>{
     if(!err){
         console.log(res.rows);
     }
@@ -19,4 +24,24 @@ client.query(`SELECT * FROM users_data`, (err, res)=>{
         console.log(err.message)
     }
     client.end;
-})
+});
+//Información de usuario luego de crear cuenta. INSERT
+var nombre = 'Mario'
+var apellido = 'Pisquiy'
+var correo = 'mariopisquiy@ufm.edu'
+var pass = 'tutickertPas234'
+var edad = 19;
+var region = 'CentroAmerica'
+const INSERTQ = {
+    text: 'INSERT INTO public.users_data(nombre, apellido, correo, contrasena, edad, region)VALUES ($1, $2, $3, $4, $5, $6)',
+    values: [nombre, apellido, correo, pass, edad, region]
+  }
+client.query(INSERTQ, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message)
+    }
+    client.end;
+});
