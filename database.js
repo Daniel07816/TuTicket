18dfeb9865ed36bd6b4c16c4a437bcf3f224a52b
CarlosAlvarado-git <cmalvarado@ufm.edu>
2021-10-01 16:15:34 -0600

    Cambio para subir las fotos al git y manejar las fotos por medio de links

--- database.js
+++ database.js
@@ -5,16 +5,17 @@ const client = new Client({
     user: "postgres",
     port: 5432,
     password: "AdminCM21",
-    database: "postgres"
+    database: "tuticketbase"
 
 })
 
 client.connect();
+/*
 //Información de usuario luego de crear cuenta. SELECT
-var nombre = 'Carlos'
+var correo = 'cmalvarado@ufm.edu' // el correo debe ser único en la base de datos. limpiar espacios y todo minúscula. 
 const SELECTQ = {
-    text: 'SELECT nombre, apellido, correo, edad, region FROM public.users_data WHERE nombre = ($1)',
-    values: [nombre]
+    text: 'SELECT nombre, apellido, correo, region, path_foto FROM public.data_user_crt WHERE correo = ($1)',
+    values: [correo]
   }
 client.query(SELECTQ, (err, res)=>{
     if(!err){
@@ -24,14 +25,14 @@ client.query(SELECTQ, (err, res)=>{
         console.log(err.message)
     }
     client.end;
-});
+});*/
 //Información de usuario luego de crear cuenta. INSERT
-var nombre = 'Mario'
-var apellido = 'Pisquiy'
-var correo = 'mariopisquiy@ufm.edu'
+var nombre = 'Carlos'
+var apellido = 'Alvarado'
+var correo = 'cmalvarado@ufm.edu'
 var pass = 'tutickertPas234'
-var edad = 19;
 var region = 'CentroAmerica'
+var foto = ''
 const INSERTQ = {
     text: 'INSERT INTO public.users_data(nombre, apellido, correo, contrasena, edad, region)VALUES ($1, $2, $3, $4, $5, $6)',
     values: [nombre, apellido, correo, pass, edad, region]
